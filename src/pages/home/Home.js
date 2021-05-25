0c815347a9248779d39c2869a9b8ed2ab3abdf0b
patrick.barcelos <patrick.barcelos@orgsystem.com.br>
2021-05-25 05:37:07 -0300

    Add structure

---
+++ src/pages/home/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import api from "../../services/api"
+
+function Home() {
+
+  function handleGetMovies() {
+const moviest = 
+  }
+
+  return <>
+
+  </>;
+}
+
+export default Home;
---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { Route, BrowserRouter } from "react-router-dom";
+
+import Home from "./pages/home/Home";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Route component={Home} path="/" exact />
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
