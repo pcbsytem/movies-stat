79dcb9b1ba41723246515d5cb60deff64a661cb4
patrick.barcelos <patrick.barcelos@orgsystem.com.br>
2021-05-28 09:56:48 -0300

    Changes in Apps files and index

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import Routes from "./routes";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+
+import Footer from "./components/footer/Footer";
+import Store from "./store/Store";
 import "./App.css";
 
-function App() {
-  return <Routes />;
+function App(props) {
+  return (
+    <Provider store={Store}>
+      <div className="App">
+        <Routes />
+      </div>
+      <Footer />
+      <ToastContainer />
+    </Provider>
+  );
 }
 
 export default App;
