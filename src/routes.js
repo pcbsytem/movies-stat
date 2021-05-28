c18be90c4ce47d93b930ca825673ea14fcf3a264
patrick.barcelos <patrick.barcelos@orgsystem.com.br>
2021-05-28 09:59:34 -0300

    Add route rules

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,15 @@
 import { Route, BrowserRouter } from "react-router-dom";
 
+import Header from "./components/header/Header";
 import Home from "./pages/home/Home";
+import Favorite from "./pages/favorite/Favorite";
 
 const Routes = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Route component={Home} path="/" exact />
+      <Route component={Favorite} path="/favoritos" />
     </BrowserRouter>
   );
 };
