f1ee8b49154d9e00084ab74b0d6e85efa16fa1d2
patrick.barcelos <patrick.barcelos@orgsystem.com.br>
2021-05-28 09:53:53 -0300

    Add SearchInput component

---
+++ src/components/searchInput/SearchInput.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import { Icon, Input, InputGroup } from "rsuite";
+
+function SearchInput({ placeholder, action, width, ...props }) {
+  const [loading, seLoading] = useState(false);
+  const [search, setSearch] = useState("");
+  const [typingTimeout, setTypingTimeout] = useState(0);
+
+  function handleClick() {
+    action(search);
+  }
+
+  function handleChange(value) {
+    if (typingTimeout) {
+      clearTimeout(typingTimeout);
+    }
+    seLoading(true);
+    const timeout = setTimeout(() => {
+      action(value);
+      seLoading(false);
+    }, 1000);
+
+    setTypingTimeout(timeout);
+    setSearch(value);
+  }
+
+  return (
+    <InputGroup
+      {...props}
+      style={{ padding: "16px", width, maxWidth: "100%" }}
+      inside
+    >
+      <Input placeholder={placeholder} onChange={handleChange} />
+      <InputGroup.Button
+        onClick={handleClick}
+        style={{ top: "16px", right: "16px" }}
+        loading={loading}
+      >
+        <Icon icon="search" />
+      </InputGroup.Button>
+    </InputGroup>
+  );
+}
+
+export default SearchInput;
