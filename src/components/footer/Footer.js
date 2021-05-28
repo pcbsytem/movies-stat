39488119e56db781f2d69bfdaf6784433d0d16c3
patrick.barcelos <patrick.barcelos@orgsystem.com.br>
2021-05-28 09:52:10 -0300

    Add Footer component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,23 @@
+function Footer() {
+  return (
+    <div
+      style={{
+        height: "56px",
+        padding: "16px",
+        backgroundColor: "#424242",
+        textAlign: "center",
+      }}
+    >
+      <a
+        href="https://www.linkedin.com/in/patrick-cristian-barcelos-374ab233/"
+        target="_blank"
+        rel="noreferrer"
+        style={{ color: "#FFFFFF90" }}
+      >
+        Desenvolvido por <b style={{ color: "#FFFFFF" }}>Patrick C. Barcelos</b>
+      </a>
+    </div>
+  );
+}
+
+export default Footer;
