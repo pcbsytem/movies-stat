d4cf1d48d4bfd41f41a10b3b5933b46e8dfd8cbb
patrick.barcelos <patrick.barcelos@orgsystem.com.br>
2021-07-07 05:48:18 -0300

    Put search input inside and remove style

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,17 +1,42 @@
+import { useDispatch } from "react-redux";
+
+import {
+  getMoviesSearch,
+  getUpcomingMovies
+} from "../../store/modules/home/action";
+import SearchInput from "../../components/searchInput/SearchInput";
 import Logo from "../logo/Logo";
 import "./Banner.css";
 
 function Banner({ title, subTitle }) {
+  const dispatch = useDispatch();
+
+  function handleSearch(search) {
+    if (search) {
+      return dispatch(getMoviesSearch(1, search));
+    }
+
+    dispatch(getUpcomingMovies(1));
+  }
+
   return (
-    <div className="banner">
+    <header className="banner">
       <div className="bannerLogo">
         <Logo height="90px" />
       </div>
+
       <br />
       <h1>{title}</h1>
       <br />
-      <p>{subTitle}</p>
-    </div>
+      <p className="bannerSubTitle">{subTitle}</p>
+
+      <SearchInput
+        size="lg"
+        width="771px"
+        placeholder="Buscar..."
+        action={handleSearch}
+      />
+    </header>
   );
 }
 
