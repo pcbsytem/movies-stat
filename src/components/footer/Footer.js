c500488cda637265dd7a65460d8b5005a0626848
patrick.barcelos <patrick.barcelos@orgsystem.com.br>
2021-07-07 05:44:36 -0300

    Remove style and change for class

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,22 +1,17 @@
+import "./Footer.css";
+
 function Footer() {
   return (
-    <div
-      style={{
-        height: "56px",
-        padding: "16px",
-        backgroundColor: "#424242",
-        textAlign: "center",
-      }}
-    >
+    <footer className="footerContainer">
       <a
+        className="footerAuthor"
         href="https://www.linkedin.com/in/patrick-cristian-barcelos-374ab233/"
         target="_blank"
         rel="noreferrer"
-        style={{ color: "#FFFFFF90" }}
       >
-        Desenvolvido por <b style={{ color: "#FFFFFF" }}>Patrick C. Barcelos</b>
+        Desenvolvido por <b className="footerAuthorName">Patrick C. Barcelos</b>
       </a>
-    </div>
+    </footer>
   );
 }
 
