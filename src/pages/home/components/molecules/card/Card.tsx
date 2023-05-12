7fff177769cdcfbcbb3081442abcbdeac838878b
Patrick Cristian Barcelos <pcbsytem@hotmail.com>
2023-05-12 00:22:39 -0300

    Changes in card

--- src/pages/home/components/molecules/card/Card.tsx
+++ src/pages/home/components/molecules/card/Card.tsx
@@ -1,9 +1,11 @@
 import { Col, FlexboxGrid } from 'rsuite'
 import { Icon, Image } from '../../../../../commons/components'
 import { CardProps } from '../../../Home.types'
+import { colors } from './Card.constants'
 import './Card.css'
 
 export const Card = ({ movie, action }: CardProps) => {
+  const randomColor = Number(String(Math.random() * 7).slice(0, 1))
   return (
     <div className="card" onClick={() => action && action(movie)}>
       <FlexboxGrid.Item className="cardInner" as={Col} colspan={24} md={5}>
@@ -15,6 +17,11 @@ export const Card = ({ movie, action }: CardProps) => {
               className="cardWithoutImage"
               justify="center"
               align="middle"
+              style={{
+                backgroundColor: colors[
+                  randomColor as keyof typeof colors
+                ] as string
+              }}
             >
               <Icon iconName="fileImageO" />
             </FlexboxGrid>
