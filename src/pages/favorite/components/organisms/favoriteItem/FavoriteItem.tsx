1fda04df63f0f5619f5f35edf1994fdd36b291b7
Patrick <pkcs@gft.com>
2024-05-31 08:11:53 -0300

    fix: solve typing problems

--- src/pages/favorite/components/organisms/favoriteItem/FavoriteItem.tsx
+++ src/pages/favorite/components/organisms/favoriteItem/FavoriteItem.tsx
@@ -1,18 +1,18 @@
-import { Col, IconButton, FlexboxGrid, List } from 'rsuite'
-import { formatDate } from '../../../../../services/helper'
-import { useFavorite } from '../../../hooks/useFavorite'
-import { Icon, Image } from '../../../../../commons/components'
-import { FavoriteItemProps } from '../../../Favorite.types'
-import { FAVORITE_TEXT } from '../../../Favorite.constants'
-import './FavoriteItem.css'
+import { Col, IconButton, FlexboxGrid, List } from 'rsuite';
+import { formatDate } from '../../../../../services/helper';
+import { useFavorite } from '../../../hooks/useFavorite';
+import { Icon, Image } from '../../../../../commons/components';
+import { FavoriteItemProps } from '../../../Favorite.types';
+import { FAVORITE_TEXT } from '../../../Favorite.constants';
+import './FavoriteItem.css';
 
 export const FavoriteItem = ({ item }: FavoriteItemProps): JSX.Element => {
-  const { handleFavoriteRemove } = useFavorite()
+  const { handleFavoriteRemove } = useFavorite();
   return (
-    <List.Item className="favoriteItem">
-      <FlexboxGrid align="middle">
+    <List.Item className='favoriteItem'>
+      <FlexboxGrid align='middle'>
         <FlexboxGrid.Item as={Col} colspan={24} md={3}>
-          <Image path={item?.backdrop_path} title={item.title} width="100%" />
+          <Image path={item?.backdrop_path} title={item.title} width='100%' />
         </FlexboxGrid.Item>
 
         <FlexboxGrid.Item as={Col} colspan={24} md={9}>
@@ -21,25 +21,25 @@ export const FavoriteItem = ({ item }: FavoriteItemProps): JSX.Element => {
           <p>{item?.overview}</p>
         </FlexboxGrid.Item>
 
-        <FlexboxGrid.Item className="centerText" as={Col} colspan={24} md={4}>
+        <FlexboxGrid.Item className='centerText' as={Col} colspan={24} md={4}>
           <div>{FAVORITE_TEXT.VOTE_AVERAGE_TITLE}</div>
-          <div>{`${item.vote_average * 10}% `}</div>
+          <div>{`${(item.vote_average ?? 0) * 10}% `}</div>
         </FlexboxGrid.Item>
 
-        <FlexboxGrid.Item className="centerText" as={Col} colspan={24} md={4}>
+        <FlexboxGrid.Item className='centerText' as={Col} colspan={24} md={4}>
           <div>{formatDate(item?.release_date)}</div>
         </FlexboxGrid.Item>
 
-        <FlexboxGrid.Item className="endText" as={Col} colspan={24} md={4}>
+        <FlexboxGrid.Item className='endText' as={Col} colspan={24} md={4}>
           <IconButton
-            size="md"
-            appearance="subtle"
-            icon={<Icon iconName="trash2" />}
-            onClick={() => handleFavoriteRemove(item)}
+            size='md'
+            appearance='subtle'
+            icon={<Icon iconName='trash2' />}
+            onClick={() => handleFavoriteRemove({ id: item.id ?? 0 })}
             title={FAVORITE_TEXT.ICON_TITLE}
           />
         </FlexboxGrid.Item>
       </FlexboxGrid>
     </List.Item>
-  )
-}
+  );
+};
