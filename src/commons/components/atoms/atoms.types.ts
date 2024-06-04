3a9616c5fd545c214bca4cbed8a01b6b7cf12d76
Patrick <pkcs@gft.com>
2024-06-04 08:40:14 -0300

    feat: improve SearchInput

--- src/commons/components/atoms/atoms.types.ts
+++ src/commons/components/atoms/atoms.types.ts
@@ -1,34 +1,35 @@
-import { ReactNode } from "react"
-import { TypeAttributes } from "rsuite/esm/@types/common"
+import { ReactNode } from 'react';
+import { TypeAttributes } from 'rsuite/esm/@types/common';
 export interface IconProps {
-  iconName: string
-  height?: string
-  width?: string
+  iconName: string;
+  height?: string;
+  width?: string;
 }
 
 export interface SearchInputProps {
-  action: (params: string) => void
-  placeholder: string
-  size: TypeAttributes.Size
+  action: (params: string) => void;
+  placeholder: string;
+  size: TypeAttributes.Size;
+  alt: string;
 }
 
 export interface SliderProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
 export interface MovieStatProps {
-  height?: string
+  height?: string;
 }
 
 export interface ImageProps {
-  path: string
-  title: string
-  width?: string
+  path: string;
+  title: string;
+  width?: string;
 }
 
 export interface PaginationProps {
-  currentPage: number
-  handlePagination: (page: number) => void
-  maxButtons: number
-  totalCount: number
+  currentPage: number;
+  handlePagination: (page: number) => void;
+  maxButtons: number;
+  totalCount: number;
 }
--- src/commons/components/atoms/searchInput/SearchInput.tsx
+++ src/commons/components/atoms/searchInput/SearchInput.tsx
@@ -1,42 +1,47 @@
-import { useState } from 'react'
-import { Input, InputGroup, Loader } from 'rsuite'
-import SearchIcon from '@rsuite/icons/Search'
-import { SearchInputProps } from '../atoms.types'
+import { useState } from 'react';
+import { Input, InputGroup, Loader } from 'rsuite';
+import SearchIcon from '@rsuite/icons/Search';
+import { SearchInputProps } from '../atoms.types';
 
-export function SearchInput({ placeholder, action, size }: SearchInputProps) {
-  const [loading, seLoading] = useState(false)
-  const [search, setSearch] = useState('')
-  const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout>()
+export function SearchInput({
+  placeholder,
+  action,
+  size,
+  alt
+}: Readonly<SearchInputProps>) {
+  const [loading, setLoading] = useState(false);
+  const [search, setSearch] = useState('');
+  const [typingTimeout, setTypingTimeout] = useState<NodeJS.Timeout>();
 
   function handleClick() {
-    action(search)
+    action(search);
   }
 
   function handleChange(value: string) {
     if (typingTimeout) {
-      clearTimeout(typingTimeout)
+      clearTimeout(typingTimeout);
     }
-    seLoading(true)
+    setLoading(true);
     const timeout: NodeJS.Timeout = setTimeout(() => {
-      action(value)
-      seLoading(false)
-    }, 1000)
+      action(value);
+      setLoading(false);
+    }, 1000);
 
-    setTypingTimeout(timeout)
-    setSearch(value)
+    setTypingTimeout(timeout);
+    setSearch(value);
   }
 
   return (
     <InputGroup size={size}>
-      <Input
-        placeholder={placeholder}
-        onChange={handleChange}
-        alt="search input"
-        autoComplete="on"
-      />
       <InputGroup.Addon onClick={handleClick}>
         {loading ? <Loader /> : <SearchIcon />}
       </InputGroup.Addon>
+      <Input
+        placeholder={placeholder}
+        onChange={handleChange}
+        autoComplete='on'
+        alt={alt}
+      />
     </InputGroup>
-  )
+  );
 }
