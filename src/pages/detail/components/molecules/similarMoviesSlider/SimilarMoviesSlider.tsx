7471a73320221e4bd19ede8e40fdde13eb66cb94
Patrick <pkcs@gft.com>
2024-06-04 08:43:51 -0300

    fix: set key for the list

--- src/pages/detail/components/molecules/similarMoviesSlider/SimilarMoviesSlider.tsx
+++ src/pages/detail/components/molecules/similarMoviesSlider/SimilarMoviesSlider.tsx
@@ -1,19 +1,19 @@
-import { Slider } from '../../../../../commons/components'
-import { MovieProps } from '../../../../../commons/typing'
-import { Card } from '../../../../home/components'
-import { SimilarMoviesSliderProps } from '../../../Detail.types'
-import { SimilarMoviesEmptyState } from '../../index'
+import { Slider } from '../../../../../commons/components';
+import { MovieProps } from '../../../../../commons/typing';
+import { Card } from '../../../../home/components';
+import { SimilarMoviesSliderProps } from '../../../Detail.types';
+import { SimilarMoviesEmptyState } from '../../index';
 
 export const SimilarMoviesSlider = ({ list }: SimilarMoviesSliderProps) => {
   return (
     <>
       <Slider>
         {list.map((item: MovieProps, index: number) => (
-          <Card movie={item} key={index} />
+          <Card movie={item} key={`${item.id} - ${index}`} />
         ))}
       </Slider>
 
       {list.length === 0 && <SimilarMoviesEmptyState />}
     </>
-  )
-}
+  );
+};
