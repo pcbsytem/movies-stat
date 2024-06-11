4b20dc0fdb434f824ab4dd79a1ef105b593873fc
Patrick <pkcs@gft.com>
2024-06-11 10:51:32 -0300

    fix: update token const name

--- src/services/api.constants.ts
+++ src/services/api.constants.ts
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.VITE_APP_API;
-export const BEARER_TOKEN = import.meta.env.VITE_APP_BEARER_TOKEN;
+export const APP_TOKEN = import.meta.env.VITE_APP_TOKEN;
 export const IMAGE_URL = import.meta.env.VITE_APP_IMAGE;
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import { API_URL, BEARER_TOKEN } from './api.constants';
+import { API_URL, APP_TOKEN } from './api.constants';
 
 const api = axios.create({
   baseURL: API_URL,
   headers: {
     accept: 'application/json',
-    Authorization: `Bearer ${BEARER_TOKEN}`
+    Authorization: `Bearer ${APP_TOKEN}`
   }
 });
 
