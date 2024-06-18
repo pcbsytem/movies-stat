726a9c3461c976251e3753d9fdd19cd034e61f1b
Patrick <pkcs@gft.com>
2024-06-18 07:15:04 -0300

    chore: add semicolon in favorite constants

--- src/pages/favorite/Favorite.constants.ts
+++ src/pages/favorite/Favorite.constants.ts
@@ -1,15 +1,15 @@
-import { ToastPosition } from "react-toastify";
+import { ToastPosition } from 'react-toastify';
 
 export const FAVORITE_TEXT = {
   ICON_TITLE: 'Clique para remover favorito',
   VOTE_AVERAGE_TITLE: 'Relevância'
-}
-export const FAVORITE_LOCAL_STORE_KEY = 'favorite'
+};
+export const FAVORITE_LOCAL_STORE_KEY = 'favorite';
 export const FAVORITE_TOAST_TEXT = {
   REMOVED: 'Favorito removido com sucesso!',
   ADDED: 'Favorito adicionado com sucesso!',
   WITHOUT_FAVORITE: 'Nenhum favorito foi adicionado!'
-}
+};
 
 export const FAVORITE_TOAST_CONFIG = {
   position: 'top-right' as ToastPosition | undefined,
@@ -19,4 +19,4 @@ export const FAVORITE_TOAST_CONFIG = {
   pauseOnHover: true,
   draggable: true,
   progress: undefined
-}
+};
